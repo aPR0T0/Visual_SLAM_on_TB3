fe48e43e131472539150546691ad434b27380008
aPR0T0 <awshaikh_b21@me.vjti.ac.in>
2022-12-19 23:31:35 +0530

    updated BFS code with edge cases in the map and custom message

--- scripts/listener.cpp
+++ scripts/listener.cpp
@@ -1,12 +1,13 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "nav_msgs/OccupancyGrid.h"
 
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+void chatterCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  ROS_INFO("I heard: [%d]", msg->data);
 }
 
 int main(int argc, char **argv)
@@ -45,7 +46,7 @@ int main(int argc, char **argv)
    * is the number of messages that will be buffered up before beginning to throw
    * away the oldest ones.
    */
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+  ros::Subscriber sub = n.subscribe("map", 1000, chatterCallback);
 
   /**
    * ros::spin() will enter a loop, pumping callbacks.  With this version, all
