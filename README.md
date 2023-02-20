32e0afd582ed3ef8f87a4b43ef132904020dbad9
aPR0T0 <awshaikh_b21@me.vjti.ac.in>
2023-02-20 21:53:17 +0530

    Resetted according to main

--- README.md
+++ README.md
@@ -1,9 +1,35 @@
-# TurtleBot3 - BFS path planning
+# TurtleBot3 - Visual Slam and Path Planning with Dynamic Obstacle avoidance
 
 ## Objective of the project :
-To Understand path planning concepts using slam, and implementing BFS on LiDAR based TurtleBot3, and then  further increasing that knowledge and implementing more advanced path planning concepts
+To make Localization through Visual SLAM and making a local costmap using OAKD and global costmap using LIDAR on the TurtleBot3, along with this we need to implement dynamic Obstacle Avoidance
 
-## Reference for the Model:- [Robotis](https://emanual.robotis.com/docs/en/platform/turtlebot3/simulation/)
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+- [<span style="color:black">Project</span>](#objective-of-the-project)
+  - [<span style="color:purple">Demo</span>](#demo)
+  - [<span style="color:purple">Table of Contents</span>](#table-of-contents)
+  - [<span style="color:purple">Stages</span>](#stages)
+  - [<span style="color:purple">Demo</span>](#demo)
+  - [<span style="color:purple">Algorithm Flowchart</span>](#algorithm-for-bfs)
+  - [<span style="color:purple">Our Approach</span>](#pseudocode:using-stacks)
+  - [<span style="color:purple">Getting Started</span>](#how-to-run-bfs-from-this-project)
+
+### Stages
+* Stage 1:
+  - [x] Understanding Various Path Planning Algorithms
+  - [x] Writing a brute Code for BFS
+  - [x] Using LIDAR to create a map and then use MOVE_BASE along with a custom GLOBAL PLANNER for BFS
+  - [x] Implement BFS in Simulation with Global/Static Map 
+* Stage 2:
+  - [ ] Mapping and Localization through OAK-D
+  - [ ] Implementing Algorithm for Dynamic Obstacle Avoidance in simulation
+  - [ ] Creating an Arena for the same
+
+
+  
+ 
+* Reference for the Model : [Robotis](https://emanual.robotis.com/docs/en/platform/turtlebot3/simulation/)
 
 We are Using turtle bot burger for our Path planning objective
 
@@ -19,7 +45,7 @@ https://user-images.githubusercontent.com/97826285/219851215-2b4791e2-5c9c-4097-
 
 
 
----
+
 ## Algorithm for BFS
 
 ### Basic Algorithm for BFS
@@ -32,27 +58,94 @@ nodes first, before moving to the next level neighbors.
 
 ![Algorithm Visualization](https://upload.wikimedia.org/wikipedia/commons/5/5d/Breadth-First-Search-Algorithm.gif)
 
-## Pseudocode
+## Pseudocode : Using Queues
+`Note: That we use stack and layers instead of traditional queues to implement the BFS algorithm` 
 
 ```sh
 BFS(root)
+
   Pre: root is the node of the BST
   Post: the nodes in the BST have been visited in breadth first order
   q ← queue
+
   while root = ø
+
     yield root.value
     if root.left = ø
       q.enqueue(root.left)
     end if
+
     if root.right = ø
       q.enqueue(root.right)
     end if
+
     if !q.isEmpty()
       root ← q.dequeue()
     else
       root ← ø
     end if
+
   end while
+
+end BFS
+```
+* Before Moving on to the next part we need to have a better understanding of three data structures
+  * **Vectors** : Vectors are sequence containers representing arrays that can change in size. [Click Here](https://cplusplus.com/reference/vector/vector/) to know more 
+    * We don't specify namespace in the code because there are two main things `ros` and `std` so instead we use scope resolution operator to specify the functionality
+  * **Stacks** : Stacks are a type of container adaptors with LIFO(Last In First Out) type of working, where a new element is added at one end (top) and an element is removed from that end only. [Click here](https://www.geeksforgeeks.org/stack-in-cpp-stl/) to know more.
+  * **Pairs**  : Pair is used to combine together two values that may be of different data types. Pair provides a way to store two heterogeneous objects as a single unit. It is basically used if we want to store tuples. The pair container is a simple container defined in < utility > header consisting of two data elements or objects. [Click here](https://www.geeksforgeeks.org/pair-in-cpp-stl/) to know more.
+
+## Pseudocode : Using Stacks
+
+Some Initializations:
+* **path** : vector of pair, where two elements in the pair are the ith and jth index respectively
+* **layer** : vector of pair of pairs, where one pair is for the parent node indices and the other pair is for child's node index
+* **stack** : stack of vector of pair of pairs, basically used to store layers
+
+`Note : Layer contains the elements which lie in the same level.` 
+```sh
+BFS(root)
+
+  Pre: root is the node of the BST
+  Post: the nodes in the BST have been visited in breadth first order
+  st ← stack : which will store individual layers
+  layer ← layer : vector of pair of pairs, each pair contains pairs of indices of parent node and current node respectively
+
+  // Creating a tree
+  while root != final index:
+
+    check for the neighbours in east, south, north and west directions
+      layer = children which are unvisited, and then mark them visited
+    
+    st.push(layer) ← adding the whole layer
+
+    clear layer ← After this new layer begins as the previous layer is already pushed into the stack and marked visited
+
+  end while
+
+  if root == final index:
+
+    path.push_back(final index)
+    while stack is not empty:
+
+      temp_layer = st.top() ← get the topmost layer in the stack because it definitely will contain the final index
+      x = find final index in temp layer ← this will give us the node we were looking for
+      
+      if (found):
+        
+        parent = temp_layer[x].first ← Now get the parent of the and put that in the path
+        path.push_back(parent) ← parent is now in the path
+
+      end if
+
+      st.pop()
+
+    end while
+
+  end if
+
+  return(path)
+
 end BFS
 ```
 
@@ -63,13 +156,62 @@ end BFS
 * Testing bipartiteness of a graph.
 
 
-----
 ## Our use case
 #### There are two main steps that we need to perform here:
 - Storing each [node]() by connecting them to its neighbours which are unoccupied and unvisited. The subpart for this is: 
     1. Storing a single parent of current node.
     2. Storing all the unvisited neighbours as children in the form of a list.
 - After storing, we just need to reach the required node, that will be set as target and will be given as the input by the user
+
+## How to run BFS from this project
+
+1. In the first terminal source the cloning repo in the src folder of the workspace that you have created `git clone https://github.com/aPR0T0/TurtleBot-V3-BFS.git`
+
+```
+
+nano ~/.bashrc
+// And add these lines and the source the bashrc
+alias get_tb3='source /opt/ros/noetic/setup.bash && export TURTLEBOT3_MODEL=burger && source ~/your_ws/devel/setup.bash'
+
+```
+2. As we have now created an alias so no need to repeat the previous step as you relaunch the nodes, just use the alias to source the directories!
+
+```
+// alias
+get_tb3
+
+roslaunch turtlebot3_gazebo turtlebot3_world.launch
+
+// In 3rd terminal
+get_tb3
+
+roslaunch turtlebot3_slam turtlebot3_slam slam_method:=gmapping
+
+// In 4th terminal
+get_tb3
+
+roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch 
+
+// Now, just move the bot until whole arena is traversed and then close 4th terminal as map is not created
+// Once done mapping close terminal 3 and go to next instruction
+
+// In 5th terminal
+get_tb3
+
+roslaunch turtlebot3 map_node.launch map_file:=$HOME/map.yaml
+
+// Now use Estimated pose Icon on GUI to give initial estimate of where bot is...
+// then in 6th terminal
+get_tb3
+
+roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch 
+
+// traverse the map until all particles closer and closer to the bot
+
+```
+
+Voila! You got the simulation done!!
+Now, Just click on nav_goal_2d in RVIZ GUI and see the magic of path planning
 
 ## References
 
---
+++ turtlebot3/src/bfs_server.cpp
@@ -0,0 +1,428 @@
+// ############### Including headers ################### //
+
+#include "tf/tf.h"
+#include "ros/ros.h"
+#include "ros/console.h"
+#include "std_msgs/String.h"
+#include "nav_msgs/Odometry.h"
+#include "std_msgs/Int8MultiArray.h"
+#include "pp_msgs/PathPlanningPlugin.h"
+#include "pp_msgs/PathPlanningPluginResponse.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "nav_msgs/OccupancyGrid.h"
+#include "geometry_msgs/Point.h"
+#include "turtlebot3/Points.h"
+#include "bits/stdc++.h"
+#include "algorithm"
+
+// #################################################### //
+
+// ## Pre-initialzed Variables uses for heights and Lengths of the map ## //
+
+int rows = 96;
+int columns = 96;
+int data1[96*96];
+
+// ###################################################################### //
+
+
+// ################# Position based Variables ########################### //
+
+float curr_x, curr_y, curr_theta;
+float des_x, des_y, des_theta;
+
+// ###################################################################### //
+
+
+// ########################## Counters ############################## //
+
+int odom_sub_count = 0;        // Keeps track whether the odometry topic is subscribed or not
+bool final_index_found = 0;    // Keeps track whether the final index is found or not
+int path_pub_count = 0;        // Keeps track whether the path has already been created or not
+int tar_sub_count = 0 ;        // Keeps track whether the final index is available or not
+
+// ################################################################## //
+
+
+// ################################ Important Data Types ########################################### //
+
+std::vector<std::pair<int,int>> path; // This is going to be the path we traversed
+
+std::vector<std::pair<std::pair<int,int>, std::pair<int,int>>> layer; // Will be storing parent and the current node together in the single unvisited layer
+
+std::stack<std::vector<std::pair<std::pair<int,int>, std::pair<int,int>>>> stack_layer;
+
+int visited[96][96]={-1}; // Marking all nodes as unvisited
+
+// ################################################################################################## //
+
+
+// ################################ Functions for One time Use ###################################### //
+
+/*
+  Function      :  Current Map
+  Arguments     :  Message of type nav_msgs/Occupancy Grid by subscibing to the map topic
+  Functionality :  Get's the Message of the Map topic and stores it in the Map data
+  Returns       :  None
+*/
+void currentMap(const nav_msgs::OccupancyGrid::ConstPtr &msg)
+{
+
+  rows = msg->info.width;
+  columns = msg->info.height;
+  for(int i = 0 ; i < rows*columns ; i++){
+    
+    data1[i] = (msg->data[i]);
+  } 
+} 
+
+/*
+  Function      :  get position
+  Arguments     :  Message of tupe nav_msgs/Odometry by subscribing to the "/odom" Topic
+  Functionality :  Stores the Current position of the bot in the curr_x and curr_y
+  Returns       :  None
+*/
+void get_position(const nav_msgs::OdometryPtr& msg){
+  odom_sub_count++;
+  curr_x = msg->pose.pose.position.x;
+  curr_y = msg->pose.pose.position.y;
+}
+
+/*
+  Function      :  get 2d position
+  Arguments     :  map in the form of 'array - 1D' 
+  Functionality :  Converts 1D to a 2D array for better post-processing
+  Returns       :  None
+*/
+void get_2d_map(int data1[]){
+
+  for(int i = 0; i < rows ; i++){
+    for(int j = 0; j < (columns-1) ; j++){
+      visited[i][j] = data1[i*rows+j+1];
+    }
+  }
+
+}
+
+/*
+  Function      :   distance to pixels
+  Arguments     :   1. Float distance in x
+                    2. Float distance in y
+  Functionality :   Converts distance in meters to pixel indices according to the resolution of the map
+  Returns       :   A pair of integers know as "Indices" 
+*/
+std::pair<int,int> distance_to_pixel(float distance_x, float distance_y){
+  // 1 pixel = 0.1 meter
+  std::pair<int,int> indices;
+
+  indices.first  = (distance_x + 5)/0.1 ;// origin of the map is position (-5,-5)
+  indices.second = (distance_y + 5)/0.1 ;// origin of the map is position (-5,-5)
+
+  return indices;
+}
+
+/*
+  Function      :   Pixels to Distance
+  Arguments     :   1. integer index in rows
+                    2. integer index in columns
+  Functionality :   Converts  pixel indices to distance in meters according to the resolution of the map
+  Returns       :   A pair of "doubles" know as "Co-ordinates" 
+*/
+std::pair<double,double> pixel_to_distance(int index_1, int index_2){
+
+  std::pair<double,double> pixels;
+  
+  pixels.first = index_1*0.1 - 5;
+  pixels.second = index_2*0.1 - 5;
+
+  return pixels;
+}
+
+/*
+  Function      :   Layer with the help of BFS
+  Arguments     :   1. integer index in rows
+                    2. integer index in columns
+  Functionality :   Keeps a track of all the unvisited neighbours in the form of layer
+  Returns       :   None 
+*/                 
+void BFS_stack_builder(int i, int  j){
+
+  visited[i][j] = 1; // Marking current node as visited too
+
+// East
+  if(visited[i+1][j] == 0){  
+
+    if( i+1 < rows ){ // Seeing whether the index is even feasible or no
+      layer.push_back({{i,j},{i+1,j}}); // Parent is go ing to be its current node
+      visited[i+1][j] = 1;
+    }
+
+  }
+
+// South
+  if(visited[i][j+1] == 0){
+    
+    if( j+1 < columns ){
+      layer.push_back({{i,j}, {i, j+1}}); // Parent is going to be its current node
+      visited[i][j+1] = 1;
+    }
+
+  }
+
+// West
+  if(visited[i-1][j] == 0 ){
+    
+    if( i-1 >= 0 ){
+      layer.push_back({{i,j},{i-1,j}});
+      visited[i-1][j] = 1;
+    }
+
+  }
+
+// North
+  if(visited[i][j-1] == 0){
+    
+    if( j-1 >= 0 ){
+      layer.push_back({{i,j},{i,j-1}});
+      visited[i][j-1] = 1;
+    }
+
+  }
+}
+
+/*
+  Function      :   Tree Building with the help of BFS layer builder
+  Arguments     :   1. integer index in counter
+                    2. integer index in columns
+  Functionality :   Keeps a track of all the unvisited neighbours in the form of layer
+  Returns       :   None 
+*/   
+std::pair<int,int> build_a_tree( int layer_element_count, int flag, std::pair<int,int> final_index){
+
+  std::pair<int,int> indx;        // Current index 
+  std::pair<int,int> indx_parent; // Current indexs' Parents
+
+  while(!layer.empty()){ 
+
+    indx = layer.front().second; 
+
+    if(indx == final_index){
+
+      final_index_found = 1;
+      indx_parent = layer.front().first;
+      break;
+
+    }
+
+    layer.erase(layer.begin()); // This will clear the whole layer when the whole for loop is done traversing without deleting the newly added elements of the next layer
+    
+    BFS_stack_builder(indx.first, indx.second); // Now building the graph further
+    
+    if(layer_element_count == flag){
+
+      flag = layer.size();
+      layer_element_count = 0;
+      stack_layer.push(layer);
+
+    } 
+
+    layer_element_count++;
+
+  } 
+
+  return indx_parent;
+}
+
+/*
+  Function      :   Build a from stack
+  Arguments     :   1. Pair of integers for the final index
+                    2. pair of integers for the current index's parent
+  Functionality :   Build a path
+  Returns       :   None 
+*/   
+void build_path_from_stack(std::pair<int,int> final_index, std::pair<int,int> indx_parent){
+
+  if(path_pub_count >= 0){ // Cheching whether the path has been previously built or not
+
+    path.push_back(final_index); // Pushing the Final Index
+    stack_layer.pop();
+    path.clear();
+    std::cout<< "Final index was found\n"; // Just to know whether we have been successful or not
+    std::vector<std::pair<std::pair<int,int>, std::pair<int,int>>> templayer; // This layer just stores the top of the stack elements
+
+    while(!stack_layer.empty()){ // Till we reach the initial index
+
+      std::cout<<"indx parent : "<<indx_parent.first<< " " << indx_parent.second<<std::endl;
+      templayer = stack_layer.top();
+
+      while(templayer.front().second != indx_parent and !templayer.empty()){
+
+        templayer.erase(templayer.begin()); // Just a type of searching
+
+      } 
+
+      path.push_back(templayer.front().second);
+      indx_parent = templayer.front().first;
+      stack_layer.pop();
+
+    }
+  }
+  layer.clear();
+  path_pub_count++;
+
+}
+
+/*
+  Function      :   Make a Plan 
+  Arguments     :   start index, end index, width, height, resolution
+  Functionality :   Implement BFS
+  Returns       :   Path
+*/
+
+turtlebot3::Points bfs(int start_index, int end_index, int width, int height, float resolution, std::vector<int> costmap){
+
+  ROS_INFO("+-----------------+ +++++++ +-------------------+\n");
+  ROS_INFO("+-----------------+ Running +-------------------+\n");
+  ROS_INFO("+-----------------+ +++++++ +-------------------+\n");
+
+  std::pair<int,int> initial_index, final_index; // pixels of the current grid cell
+
+
+  final_index.first    = (  end_index / width  ); // Need to verify this
+  final_index.second   = (  end_index % height );
+
+  initial_index.first  = ( start_index / width );
+  initial_index.second = ( start_index % width );
+
+  ROS_INFO("end : %d   , end : %d\n", final_index.first, final_index.second);
+  ROS_INFO("start : %d , start : %d\n", initial_index.first, initial_index.second);
+  
+  get_2d_map(data1); //got the 2d map to operate upon
+
+  turtlebot3::Points P_msg; // This is going to be the final published path
+  geometry_msgs::Point  point_pub;
+
+  if(odom_sub_count > 0){ // Just checking if the subscription is done or not
+
+    visited[initial_index.first][initial_index.second] = 1; // Marking the Current node as visited
+
+    layer.push_back({{-1,-1},{initial_index.first,initial_index.second}});  // Initialzing layer0 with the First node later the elements will be added into it
+    stack_layer.push(layer);  // Now, Stack is a collection of all layers till the final index
+
+    int flag = layer.size()-1; // Flag is the signal that all the possible children in the current array has been added and now u can move on to the next layer
+
+    int layer_element_count = 0; // this will check if all the elements in the layer has been traversed or not
+
+    // ############################################################################################### //
+    std::pair<int,int> indx_parent;
+    indx_parent = build_a_tree(flag, layer_element_count, final_index); // This will give us the parent of the final node 
+
+    if(final_index_found){
+
+      build_path_from_stack(final_index, indx_parent);
+
+    } 
+    else{
+      return P_msg;
+    }
+    // Now as there is some path existing 
+
+  }
+
+  // ############### Converting the path in terms of the message that we are going to publish ############# //
+  int x_t = path.size();
+
+  geometry_msgs::Point path_pub[x_t];
+
+  for(int i = 0; i < path.size() ; i++){
+    std::pair<double,double> path_temp;
+    path_temp = pixel_to_distance(path[i].first, path[i].second);
+    
+    point_pub.x = path_temp.first;
+    point_pub.y = path_temp.second;
+    path_pub[i] = point_pub; 
+  }
+
+  P_msg.points.clear();
+  P_msg.end_index = des_theta;
+  int index_z = 0;
+  // Now declaring the messages with the help of the points
+  for (auto &it : path_pub) {
+      geometry_msgs::Point point;
+      point.x = (it).x;
+      point.y = (it).y;
+      point.z = 0;
+      P_msg.points.push_back(point);
+      index_z++;
+  }
+  return P_msg;
+}
+
+/*
+  Function      :   makePlan - A standard node for different algorithms
+  Arguments     :   Service Request, Service Response
+  Funcitonality :   Publish a path for move_base
+  Returns       :   bool that describes whether path is beign published or not
+*/
+bool  make_plan(pp_msgs::PathPlanningPlugin::Request &req, pp_msgs::PathPlanningPlugin::Response &res){
+  std::vector<int> costmap;
+  std::vector<int> path_bfs;
+  costmap = req.costmap_ros;
+
+  int width = req.width;
+  int height = req.height;
+
+  int start_index = req.start;
+  int end_index   = req.goal;
+  float resolution = 0.1;
+
+  turtlebot3::Points path_from_bfs = bfs(start_index, end_index, width, height, resolution, costmap);
+
+  if(path_from_bfs.points.size() == 0){
+    ROS_INFO("!!! NO PATH FOUND !!!\n");
+    return false;
+  }
+  else{
+    ROS_INFO("!!! PATH FOUND !!!\n");
+  }
+
+  path_bfs.clear();
+  for (auto &itx : path_from_bfs.points ){
+
+    std::pair<int,int> idx_2d = distance_to_pixel(itx.x, itx.y);
+    ROS_INFO(" x : %d  y : %d \n", idx_2d.first, idx_2d.second);
+    int idx_1d = idx_2d.first * width  +  idx_2d.second;
+    path_bfs.push_back(idx_1d);
+
+    std::reverse(path_bfs.begin(), path_bfs.end());
+  } 
+
+  res.plan = path_bfs;
+
+  return true;
+}
+
+// ##################################################################################################### //
+// #####################################     Main Function    ########################################## //
+// ##################################################################################################### //
+
+int main(int argc, char **argv) {
+  
+  // ########################## Creating Ros nodes, Subsciber, and Publishers ########################## //
+  ros::init(argc, argv, "bfs_server");
+  ros::NodeHandle n;      // Creating a nodehandler to make relevant subscribers and publishers
+
+  ros::Subscriber subscriber = n.subscribe("map",1000, currentMap);   // Subscribing to map topic
+  ros::Subscriber odo_sub = n.subscribe("odom", 1000, get_position);  // Subscribing to odom topic
+
+  ros::ServiceServer service = n.advertiseService("/move_base/GlobalPlanner/make_plan", make_plan);
+
+  ROS_INFO("Ready to publish the path\n");
+
+  ros::spin();
+  return 0;
+}
+
+// ###################################################################################################### //
+// #############################################  The End  ############################################## //
+// ###################################################################################################### //
